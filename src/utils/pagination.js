52e9784642c9d48b3c6d85bcd171efc75aa57f42
Guido Debandi <guido.debandi@claro.com.ar>
2023-09-09 17:06:28 -0300

    agregado usuario a get Publications

--- src/utils/pagination.js
+++
@@ -1,9 +0,0 @@
-
-
-export async function getPaginatedData(model, pageNumber, recordsPerPage,orderBy,attributes,include) {
-  const offset = (pageNumber - 1) * recordsPerPage;
-  const limit = recordsPerPage;
-  const order = [[orderBy, 'DESC']];
-
-  return  model.findAll({ offset, limit, order,attributes ,include}) ;
-}
